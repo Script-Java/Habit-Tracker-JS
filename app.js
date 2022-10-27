4d6494970be1f28a68575b730d05b9d371e83c88
Atrin Shahroudi <68357178+Script-Java@users.noreply.github.com>
2022-10-26 19:35:41 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("todo_name");
+const add = document.getElementById("add");
+const todo = document.getElementsByClassName("todo");
+
+add.addEventListener("click", ()=> {
+    
+})
