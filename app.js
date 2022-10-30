b85c7923cb47aef2d02690ea35e483f98bbd9ce2
Atrin Shahroudi <68357178+Script-Java@users.noreply.github.com>
2022-10-30 13:51:11 -0500

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById("todo_name");
-const add = document.getElementById("add");
-const todo = document.getElementsByClassName("todo");
-
-add.addEventListener("click", ()=> {
-    
-})
